60edb3200e6f81bc983ddf917e1059054f4ffada
luvouo <65487925+luvouo@users.noreply.github.com>
2026-02-01 13:10:46 +0900

    Update crypto-utils.js

--- core/crypto-utils.js
+++ core/crypto-utils.js
@@ -1,1 +1,8 @@
+export async function workspaceIdFromPasscode(passcode) {
+  const SALT = "bora-widget-v1";
+  const input = new TextEncoder().encode(`${SALT}:${passcode}`);
+  const hashBuf = await crypto.subtle.digest("SHA-256", input);
+  const hashArr = Array.from(new Uint8Array(hashBuf));
+  return hashArr.map(b => b.toString(16).padStart(2, "0")).join("");
+}
 
