c6ada8bdf154388280a5aec55d07f52e2027650a
luvouo <65487925+luvouo@users.noreply.github.com>
2026-02-01 14:04:10 +0900

    Update env.js

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 window.__ENV__ = {
-  SUPABASE_URL: "https://YOUR_PROJECT_REF.supabase.co",
-  SUPABASE_ANON_KEY: "YOUR_ANON_PUBLIC_KEY"
+  SUPABASE_URL: "https://oegiiszccisrrbeevdhm.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9lZ2lpc3pjY2lzcnJiZWV2ZGhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4OTg0MTIsImV4cCI6MjA4NTQ3NDQxMn0.o38uX1pv6aJwo9MlmaFwFb_LVJVbqIhMQ0hIWnL9nW8"
 };
 
